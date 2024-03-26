36939a36ff08dd9449b796470297f9025b432d1a
foreverNP <levsegeenko@gmail.com>
2024-03-26 22:06:15 +0300

    [feat] add config

---
+++ config/config.hpp
@@ -0,0 +1,82 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <map>
+#include <stdexcept>
+
+class Config
+{
+private:
+    std::map<std::string, std::string> config;
+
+    Config()
+    {
+        std::ifstream file("config/config.txt");
+        if (!file.is_open())
+        {
+            throw std::runtime_error("Cannot open config file: config.txt");
+        }
+
+        std::string line;
+        while (std::getline(file, line))
+        {
+            std::istringstream iss(line);
+            std::string key, value;
+            if (std::getline(iss, key, '=') && std::getline(iss, value))
+            {
+                config[key] = value;
+            }
+        }
+
+        file.close();
+    }
+
+public:
+    static Config &getInstance()
+    {
+        static Config instance;
+        return instance;
+    }
+
+    static std::string getString(const std::string &key)
+    {
+        auto &instance = getInstance();
+
+        auto it = instance.config.find(key);
+        if (it != instance.config.end())
+        {
+            return it->second;
+        }
+        else
+        {
+            throw std::runtime_error("Key not found in config file: " + key);
+        }
+    }
+
+    static int getInt(const std::string &key)
+    {
+        auto &instance = getInstance();
+
+        auto it = instance.config.find(key);
+        if (it != instance.config.end())
+        {
+            try
+            {
+                return std::stoi(it->second);
+            }
+            catch (const std::invalid_argument &e)
+            {
+                throw std::runtime_error("Invalid value for key in config file: " + key);
+            }
+        }
+        else
+        {
+            throw std::runtime_error("Key not found in config file: " + key);
+        }
+    }
+};
+
+#endif // CONFIG_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include "../include/park.h"
 #include "../include/driverstorer.h"
+#include "../config/config.hpp"
 
 int main()
 {
-	std::cout << sum(1, 2) << std::endl;
-	return 0;
+    std::cout << sum(1, 2) << std::endl;
+
+    std::cout << Config::getString("admine_password") << std::endl;
+    std::cout << Config::getInt("commission_fees") << std::endl;
+
+    return 0;
 }
